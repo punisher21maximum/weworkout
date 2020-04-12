deecda61226838043485e6753c55d1ddfe2c4d1b
punisher21maximum <Punishermax0021@gmail.com>
2020-04-12 20:19:42 +0530

    index view

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,10 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    paginate_by = 6
+
+
+
 
 class UserPostListView(ListView):
     model = Post
