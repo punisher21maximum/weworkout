a828ba1f3c268910e007d963cec93adf66b0e5bf
Vishal Singh <effort21pool@gmail.com>
2020-05-19 20:12:05 +0530

    initial

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-
+from ckeditor.fields import RichTextField
 class Post(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100)
-	content = models.TextField()
+	# content = models.TextField()
+	content = RichTextField()
 	date_posted = models.DateTimeField(default=timezone.now)
 	last_edit = models.DateTimeField(auto_now=True)
 	likes = models.IntegerField(default=0)
--- weworkout/settings.py
+++ weworkout/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
 
     'crispy_forms',
+    'ckeditor',
 
     'django.contrib.admin',
     'django.contrib.auth',
