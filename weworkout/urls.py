fdfd1e51fd1173391d7722fd7e1816278f28ad4d
Vishal Singh <effort21pool@gmail.com>
2020-05-24 01:29:39 +0530

    worked on forms UI

--- weworkout/urls.py
+++ weworkout/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('activate/<uidb64>/<token>/',
+        user_views.activate, name='activate'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
